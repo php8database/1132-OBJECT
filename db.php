54403f0d72970fd0180c6ec44de24f7e61830907
php8database <php8database@gmail.com>
2024-11-26 15:22:29 +0800

    完成find()函式

--- db.php
+++ db.php
@@ -37,6 +37,18 @@ class DB{
         return $this->fetchAll($sql);
     }
 
+    function find($id){
+        $sql="SELECT * FROM $this->table ";
+
+        if(is_array($id)){
+            $where=$this->a2s($id);
+            $sql=$sql . " WHERE ". join(" && ",$where);
+        }else{
+            $sql .= " WHERE `id`='$id' ";
+        }
+        return $this->fetchOne($sql);
+    }
+
     
     /**
      * 把陣列轉成條件字串陣列
@@ -52,12 +64,12 @@ class DB{
 
     function fetchOne($sql){
         //echo $sql;
-        return $this->pdo->query($sql)->fetch();
+        return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
     }
     
     function fetchAll($sql){
         //echo $sql;
-        return $this->pdo->query($sql)->fetchAll();
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
     
 }
@@ -76,7 +88,7 @@ function dd($array){
 $DEPT=new DB('dept');
 
 //$dept=$DEPT->q("SELECT * FROM dept");
-$dept=$DEPT->all(" Order by `id` DESC");
+$dept=$DEPT->find(['code'=>'404']);
 
 dd($dept);
 
